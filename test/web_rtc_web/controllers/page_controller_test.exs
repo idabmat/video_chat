7640c46d720a7b4560633c37a963f9ed330ff86c
Igor de Alcantara Barroso <idabmat@gmail.com>
2017-10-01 16:03:24 +0100

    Fix test

--- test/web_rtc_web/controllers/page_controller_test.exs
+++ test/web_rtc_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule WebRtcWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Web RTC playground"
   end
 end
